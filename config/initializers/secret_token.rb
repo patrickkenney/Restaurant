8515f34145be3a81e13075db6d31aa221af21e84
Deviceable LLC <patrick@deviceable.com>
2013-05-09 13:28:28 -0400

    Installed Rails Added Postgres, Coffeescript, Slim, Compass, and Sass gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Offcanvas::Application.config.secret_token = '16952ac1eff4a1c6e7430395e757f4eaebf9cec514efcf8afd507a203dc132527c37e88d805cf24dbafac49466e3dc2646a2da852f9a396af522861e6a7138bf'
