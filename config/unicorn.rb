cd703c4bd3906b970a936f28cd17711d0c34b1cd
Elliot Murphy <elliot.murphy@patdeegan.com>
2013-08-20 15:17:17 -0400

    Try disabling prepared statements
    This is triggering a bunch of errors when using ActiveAdmin.
    Not confident that the unicorn after_fork code will reliably
    disable prepared statements on heroku - verify after deploying.
    
    https://github.com/rails/rails/issues/1627

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -25,6 +25,9 @@ after_fork do |server, worker|
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  if defined?(ActiveRecord::Base)
+    config = Rails.application.config.database_configuration[Rails.env]
+    config['prepared_statements'] = false
+    ActiveRecord::Base.establish_connection(config)
+  end
 end
